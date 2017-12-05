340b8da3620cc58fda5b0b152f7f46b337365443
jackdied <jackdied@gmail.com>
2017-12-05 00:15:31 -0500

    Day 5, Star 2

--- day_05/p5.py
+++ day_05/p5.py
@@ -19,7 +19,23 @@ def star1():
     return
 
 def star2():
+    regs = dict(enumerate(my_input))
+    curr = 0
+    steps = 0
+    while True:
+        try:
+            jump = regs[curr]
+        except LookupError:
+            print(steps)
+            return
+        if jump >= 3:
+            regs[curr] -= 1
+        else:
+            regs[curr] += 1
+        curr += jump
+        steps += 1
     return
+
 
 if __name__ == '__main__':
     star1()
