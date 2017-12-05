c708ba32b8a83b8ba928fdea121793b9f7d079f5
jackdied <jackdied@gmail.com>
2017-12-05 00:13:04 -0500

    Day 5, Star 1

---
+++ day_05/p5.py
@@ -0,0 +1,29 @@
+import itertools as it
+
+my_input = [int(x.strip()) for x in open('input.txt').readlines()]
+print(my_input)
+
+def star1():
+    regs = dict(enumerate(my_input))
+    curr = 0
+    steps = 0
+    while True:
+        try:
+            jump = regs[curr]
+        except LookupError:
+            print(steps)
+            return
+        regs[curr] += 1
+        curr = curr+jump
+        steps += 1
+    return
+
+def star2():
+    return
+
+if __name__ == '__main__':
+    star1()
+    # not 52531
+    # not 1074
+    # not 364540
+    star2()
