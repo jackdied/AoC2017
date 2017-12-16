3db51e55a5de63f1ff33e634117e9de278696111
jackdied <jackdied@gmail.com>
2017-12-15 21:04:53 -0500

    Day12 stars 1&2

--- day_12/p12.py
+++ day_12/p12.py
@@ -1,11 +1,47 @@
 import itertools as it
+import re
 
 
-def star1():
-    return
+def pairs(filename='input.txt'):
+    for line in open(filename).readlines():
+        m = re.match('(\d+) <-> (.*)', line)
+        start, rest = m.groups()
+        left = int(start)
+        rest = [int(x.strip()) for x in rest.split(',')]
+        yield left, rest
 
-def star2():
-    return
+def star1(edges, start=0):
+    d = dict(edges)
+    dist = {}  # {end: dist_from_start}
+    stack = [(start, 0)]  # (curr, dist)
+    while stack:
+        curr, n = stack.pop(0)
+        for other in d.get(curr, []):
+            if other in dist:
+                continue
+            stack.append((other, n+1))
+            dist[other] = n+1
+    print("REACHABLE", len(dist))
 
-star1()
-star2()
+def star2(edges):
+    d = dict(edges)
+    groups = []
+    while d:
+        dist = {}  # {end: dist_from_start}
+        groups.append(dist)
+        start = next(iter(d))
+        stack = [(start, 0)]  # (curr, dist)
+        dist[start] = 0
+        while stack:
+            curr, n = stack.pop(0)
+            for other in d.pop(curr, []):
+                if other in dist:
+                    continue
+                stack.append((other, n+1))
+                dist[other] = n+1
+
+    print("GROUPS", len(groups), [len(g) for g in groups])
+
+
+star1(pairs())
+star2(pairs())
